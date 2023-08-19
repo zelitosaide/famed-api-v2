a00fc7697191757e44999bce4ab36c0425334303
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-19 13:20:16 +0200

    Links

--- src/links/schemas/link.schema.ts
+++ src/links/schemas/link.schema.ts
@@ -1,1 +1,9 @@
+import { Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+export type LinkDocument = HydratedDocument<Link>;
+
+@Schema({ timestamps: true })
 export class Link {}
+
+export const LinkSchema = SchemaFactory.createForClass(Link);
