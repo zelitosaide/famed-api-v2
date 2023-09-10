7e2e5d82fcda47371f50733dc5a580353791d01d
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-10 09:57:07 +0200

    Contents

--- src/contents/contents.module.ts
+++ src/contents/contents.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ContentsService } from './contents.service';
 import { ContentsController } from './contents.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Content, ContentSchema } from './schemas/content.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Content.name, schema: ContentSchema }]),
+  ],
   controllers: [ContentsController],
   providers: [ContentsService],
 })
