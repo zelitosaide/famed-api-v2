a8f5387296f3461ac9f20c91b48384ad3ce67d7a
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-11 15:34:11 +0200

    Files

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { LinksModule } from "./links/links.module";
 import { ContentsModule } from "./contents/contents.module";
 import { FilesModule } from "./files/files.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({
   imports: [
@@ -13,6 +15,10 @@ import { FilesModule } from "./files/files.module";
     LinksModule,
     ContentsModule,
     FilesModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "uploads"),
+      serveRoot: "/uploads"
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
