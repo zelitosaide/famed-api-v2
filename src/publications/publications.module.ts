0849d31a2c6e9d017efb2ca1348eb6eb0e669785
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-25 13:11:11 +0200

    Publications

--- src/publications/publications.module.ts
+++ src/publications/publications.module.ts
@@ -1,8 +1,15 @@
 import { Module } from "@nestjs/common";
 import { PublicationsService } from "./publications.service";
 import { PublicationsController } from "./publications.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Publication, PublicationSchema } from "./schemas/publication.schema";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Publication.name, schema: PublicationSchema },
+    ]),
+  ],
   controllers: [PublicationsController],
   providers: [PublicationsService],
 })
