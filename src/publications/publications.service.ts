bb2a1bf389d3ec9e3e6a94c4a73196ea8f1f8397
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-25 13:13:29 +0200

    Publications

--- src/publications/publications.service.ts
+++ src/publications/publications.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { CreatePublicationDto } from "./dto/create-publication.dto";
 import { UpdatePublicationDto } from "./dto/update-publication.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Publication } from "./schemas/publication.schema";
+import { Model } from "mongoose";
 
 @Injectable()
 export class PublicationsService {
+  constructor(
+    @InjectModel(Publication.name) private publicationModel: Model<Publication>,
+  ) {}
+
   create(createPublicationDto: CreatePublicationDto) {
     return "This action adds a new publication";
   }
