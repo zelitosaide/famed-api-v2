e05b3c400a62b0742cb2bbdc8b35af81efc87f61
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-26 08:55:35 +0200

    Projects

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,1 +1,13 @@
-export class CreateProjectDto {}
+export class CreateProjectDto {
+  title: string;
+  description: string;
+  regNumBioethic: string;
+  department: string;
+  approvalDate: Date;
+  content: string;
+  thumbnail: string;
+  team: Object[];
+  financiers: Object[];
+  projectStartDate: Date;
+  projectEndDate: Date;
+}
