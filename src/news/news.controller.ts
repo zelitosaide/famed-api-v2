4906c4f411a535fbf8d29e5d48f2f86875e9d1e8
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-27 00:07:13 +0200

    News

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -6,28 +6,52 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  UploadedFile,
 } from "@nestjs/common";
 import { NewsService } from "./news.service";
 import { CreateNewsDto } from "./dto/create-news.dto";
 import { UpdateNewsDto } from "./dto/update-news.dto";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import { News } from "./schemas/new.schema";
 
 @Controller("news")
 export class NewsController {
   constructor(private readonly newsService: NewsService) {}
 
   @Post()
-  create(@Body() createNewsDto: CreateNewsDto) {
-    return this.newsService.create(createNewsDto);
+  @UseInterceptors(
+    FileInterceptor("image", {
+      storage: diskStorage({
+        destination: "uploads/news/images",
+        filename(req, file, callback) {
+          const uniquePreffix =
+            Date.now() + "-" + Math.round(Math.random() * 1e9);
+          callback(null, uniquePreffix + "-" + file.originalname);
+        },
+      }),
+    }),
+  )
+  async create(
+    @Body() createNewsDto: CreateNewsDto,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    return await this.newsService.create({
+      title: createNewsDto.title,
+      description: createNewsDto.description,
+      image: image.path,
+    });
   }
 
   @Get()
-  findAll() {
+  async findAll(): Promise<News[]> {
     return this.newsService.findAll();
   }
 
   @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.newsService.findOne(+id);
+  async findOne(@Param("id") id: string): Promise<News> {
+    return this.newsService.findOne(id);
   }
 
   @Patch(":id")
@@ -36,7 +60,7 @@ export class NewsController {
   }
 
   @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.newsService.remove(+id);
+  async remove(@Param("id") id: string) {
+    return this.newsService.remove(id);
   }
 }
