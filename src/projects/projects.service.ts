ab8431c192e261f487b04728b41c9e4da5e54946
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-26 09:11:34 +0200

    Projects

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { CreateProjectDto } from "./dto/create-project.dto";
 import { UpdateProjectDto } from "./dto/update-project.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Project } from "./schemas/project.schema";
 
 @Injectable()
 export class ProjectsService {
+  constructor(@InjectModel(Project.name) private projectModel: Model<Project>) {}
+
   create(createProjectDto: CreateProjectDto) {
     return "This action adds a new project";
   }
